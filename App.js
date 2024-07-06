cc67544a2a390dbfcc5723997c4e7ddeeb2cf814
kagenza isaac <kagisaac103@gmail.com>
2024-07-07 00:12:42 +0200

    still fixing conflict in the parckeges

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
@@ -6,12 +5,3 @@ import AppNavigator from "./src/navigation/AppNavigator";
 export default function App() {
 	return <AppNavigator />;
 }
-=======
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import AppNavigator from "./src/navigation/AppNavigator";
-
-export default function App() {
-	return <AppNavigator />;
-}
->>>>>>> 4f1fca7 (adding full ferltring functionality to the order screen)
--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
 };
-=======
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
->>>>>>> 4f1fca7 (adding full ferltring functionality to the order screen)
