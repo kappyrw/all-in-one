f36c1d1711c65a3ffa10f53c732c008b467f595d
kagenza isaac <kagisaac103@gmail.com>
2024-07-06 23:30:44 +0200

    fixing merginging issues

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
@@ -5,3 +6,12 @@ import AppNavigator from "./src/navigation/AppNavigator";
 export default function App() {
 	return <AppNavigator />;
 }
+=======
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+export default function App() {
+	return <AppNavigator />;
+}
+>>>>>>> 4f1fca7 (adding full ferltring functionality to the order screen)
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
 };
+=======
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
+>>>>>>> 4f1fca7 (adding full ferltring functionality to the order screen)
